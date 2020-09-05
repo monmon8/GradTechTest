c71c138b8c61846e3283866fe8117831f710ad2a
Ahmed <imaneben8@gmail.com>
2020-09-05 13:44:41 +0100

    initial setup and breakdown

---
+++ createMenuData.js
@@ -0,0 +1,7 @@
+// Small breakdown of my thought process:
+// 1. create an object to store the result
+// 2. map through data
+// 3. split each string at the first '/'
+// 4. check for pre-existing parent in the result array
+// 5. if the parent exists, then push the child into the data array
+// 6. if the parent doesn't exist, then create the parent and add the child into a new data array
